d41b50a47fb9e9d356339934c06c317451ba9f96
Unnur Sól Ingimarsdóttir <Unnur Sl Ingimarsdttir>
2016-10-11 20:32:05 +0000

    New line test

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,8 +6,8 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
-		  String[] numbers = text.split(",");
+		else if(text.contains(",") | text.contains("\n")){
+		  String[] numbers = text.split(",|\n");
 		  int total = 0;
 		  for(String number : numbers){
 		    total += Integer.parseInt(number);
