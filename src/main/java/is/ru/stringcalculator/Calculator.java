1f67f201f83d890330aa408ecf2e3e2456f203d3
Unnur Sól Ingimarsdóttir <Unnur Sl Ingimarsdttir>
2016-10-11 20:06:02 +0000

    Test two strings

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,7 +6,10 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-
+		else if(text.contains(",")){
+	 		String[] numbers = text.split(",");
+	 		return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+ 		}
 		else
 			return 1;
 	}
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -19,5 +19,8 @@ public class CalculatorTest {
 	  assertEquals(1, Calculator.add("1"));
 	}
 
-
+	@Test
+	public void testTwoStrings(){
+	  assertEquals(3, Calculator.add("1,2"));
+	}
 }
