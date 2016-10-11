515d0f55367fd5a6310f8e10c82919be7df151ca
Unnur Sól Ingimarsdóttir <Unnur Sl Ingimarsdttir>
2016-10-11 20:13:46 +0000

    Multiple number test

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -7,11 +7,14 @@ public class Calculator {
 			return 0;
 		}
 		else if(text.contains(",")){
-	 		String[] numbers = text.split(",");
-	 		return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
- 		}
+		  String[] numbers = text.split(",");
+		  int total = 0;
+		  for(String number : numbers){
+		    total += Integer.parseInt(number);
+		}
+		  return total;
+		}
 		else
 			return 1;
 	}
-
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -23,4 +23,8 @@ public class CalculatorTest {
 	public void testTwoStrings(){
 	  assertEquals(3, Calculator.add("1,2"));
 	}
+	@Test
+	public void testMultipleNumbers(){
+	    assertEquals(6, Calculator.add("1,2,3"));
+	}
 }
